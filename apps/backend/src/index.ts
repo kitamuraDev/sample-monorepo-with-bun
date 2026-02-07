736af78208d89e993817066775ad2e7eb6c62be8
kitamuraDev <masato1999824@icloud.com>
2026-02-07 23:04:43 +0900

    :sparkles: モノレポで型共有サンプル

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,9 +1,15 @@
 import { Hono } from "hono";
+import { paths } from "@api-schema"
+
+type SuccessResponse = paths["/users/{id}"]["get"]["responses"][200]["content"]["application/json"];
+const successResponseExample: SuccessResponse = {
+  id: 1,
+  name: "John Doe",
+};
 
 const app = new Hono<{ Bindings: CloudflareBindings }>();
-
 app.get("/message", (c) => {
-  return c.text("Hello Hono!");
+  return c.json(successResponseExample)
 });
 
 export default app;
